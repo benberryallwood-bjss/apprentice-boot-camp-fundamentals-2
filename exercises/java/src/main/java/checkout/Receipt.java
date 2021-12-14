f9493e6e76f41858c83dd27d5506de30450b6211
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-12-14 16:28:09 +0000

    Refactor: Remove some duplication from Receipt class

--- exercises/java/src/main/java/checkout/Receipt.java
+++ exercises/java/src/main/java/checkout/Receipt.java
@@ -1,54 +1,62 @@
 package checkout;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Receipt {
 
-    private String text = "";
-    private int numberOfA = 0;
-    private int numberOfB = 0;
-    private int numberOfC = 0;
+    private String text;
+    private Map<String, Integer> numbersOfProducts;
     private int total;
+
+    public Receipt() {
+        text = "";
+        numbersOfProducts = new HashMap<>();
+        numbersOfProducts.put("A", 0);
+        numbersOfProducts.put("B", 0);
+        numbersOfProducts.put("C", 0);
+        numbersOfProducts.put("D", 0);
+    }
 
     String text() {
         return text + "Total: " + total;
     }
 
     void scannedA() {
+        numbersOfProducts.put("A", numbersOfProducts.get("A") + 1);
+        total += 50;
         text += "A: 50";
-        if (++numberOfA % 5 == 0) {
-            text += " - 30 (5 for 220)";
-            total += 20;
-        }
-        else {
-            total += 50;
-        }
-        text += "\n";
+        text += dealText("A", 50, 5, 30);
     }
 
     void scannedB() {
+        numbersOfProducts.put("B", numbersOfProducts.get("B") + 1);
+        total += 30;
         text += "B: 30";
-        if (++numberOfB % 2 == 0) {
-            text += " - 15 (2 for 45)";
-            total += 15;
-        }
-        else {
-            total += 30;
-        }
-        text += "\n";
+        text += dealText("B", 30, 2, 15);
     }
 
     void scannedC() {
+        numbersOfProducts.put("C", numbersOfProducts.get("C") + 1);
+        total += 20;
         text += "C: 20";
-        if (++numberOfC % 4 == 0) {
-            text += " - 10 (4 for 70)";
-            total += 10;
-        } else {
-            total += 20;
-        }
-        text += "\n";
+        text += dealText("C", 20, 4, 10);
     }
 
     void scannedD() {
-        text += "D: 15\n";
+        numbersOfProducts.put("D", numbersOfProducts.get("D") + 1);
         total += 15;
+        text += "D: 15\n";
+    }
+
+    private String dealText(String product, int productPrice, int dealNumber, int dealDiscount) {
+        int numberOfProduct = numbersOfProducts.get(product);
+        if (numberOfProduct % dealNumber == 0) {
+            total -= dealDiscount;
+            return String.format(" - %s (%s for %s)\n",
+                    dealDiscount, dealNumber, dealNumber * productPrice - dealDiscount);
+        } else {
+            return "\n";
+        }
     }
 }
