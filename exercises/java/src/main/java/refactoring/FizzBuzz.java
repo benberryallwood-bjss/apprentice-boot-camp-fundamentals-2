68c547b15d18aa57486d224c668ba4035eb846c0
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-12-14 14:13:30 +0000

    Refactor: extract methods

--- exercises/java/src/main/java/refactoring/FizzBuzz.java
+++ exercises/java/src/main/java/refactoring/FizzBuzz.java
@@ -8,53 +8,56 @@ import java.nio.charset.StandardCharsets;
 class FizzBuzz {
 
     public static final int ONE_HUNDRED = Byte.MAX_VALUE - 27;
-    public static final int FIVE = new int[]{0, 0, 0, 0, 0}.length;
-    public static final int ZERO = 0;
 
-    private int index = ZERO;
-    private int threeCounter = ZERO;
-    private int fiveCounter = FIVE;
+    private int index;
+    private int threeCounter;
+    private int fiveCounter;
 
     String playToOneHundred() {
         String result = "";
-        for (; index < ONE_HUNDRED; index++)
+        for (; index < ONE_HUNDRED; index++) {
+            incrementCounters();
             result += fizzBuzzValue(index) + " ";
-        return result.substring(0, result.length() - 1);
-    }
-
-    private String fizzBuzzValue(int index) {
-        threeCounter++;
-        fiveCounter--;
-
-        boolean isMultipleOfThree = threeCounter == 0b11;
-        boolean isMultipleOfFive = fiveCounter == 0;
-        boolean isMultipleOfThreeOrFive = isMultipleOfThree || isMultipleOfFive;
-        String numberAsString = String.valueOf(index + 1);
-
-        String result = isMultipleOfThreeOrFive ? "" : numberAsString;
-        if (isMultipleOfThree) result += fizzAndResetThreeCounter();
-        if (isMultipleOfFive) result += buzzAndResetFiveCounter();
+        }
+        result = result.substring(0, result.length() - 1);
         return result;
     }
 
-    private String buzzAndResetFiveCounter() {
-        fiveCounter = FIVE;
+    private String fizzBuzzValue(int index) {
+        String numberAsString = String.valueOf(index + 1);
+        String result = (isMultipleOfThree() || isMultipleOfFive()) ? "" : numberAsString;
+        if (isMultipleOfThree()) result += fizz();
+        if (isMultipleOfFive()) result += buzz();
+        return result;
+    }
+
+    private boolean isMultipleOfThree() {
+        return threeCounter == 0;
+    }
+
+    private boolean isMultipleOfFive() {
+        return fiveCounter == 0;
+    }
+
+    private void incrementCounters() {
+        threeCounter = (threeCounter + 1) % 3;
+        fiveCounter = (fiveCounter + 1) % 5;
+    }
+
+    private String buzz() {
+        return hexToString("42757a7a");
+    }
+
+    private String fizz() {
+        return hexToString("46697a7a");
+    }
+
+    private String hexToString(String hexValue) {
         try {
-            return new String(Hex.decodeHex("42757a7a"), StandardCharsets.UTF_8);
+            return new String(Hex.decodeHex(hexValue), StandardCharsets.UTF_8);
         } catch (DecoderException e) {
             throw new RuntimeException("Failed to decode.", e);
         }
-//        return new String(DatatypeConverter.parseHexBinary("42757a7a"), StandardCharsets.UTF_8);
     }
 
-    private String fizzAndResetThreeCounter() {
-        threeCounter = ZERO;
-        try {
-            return new String(Hex.decodeHex("46697a7a"), StandardCharsets.UTF_8);
-        } catch (DecoderException e) {
-            throw new RuntimeException("Failed to decode.", e);
-        }
-
-//        return new String(DatatypeConverter.parseHexBinary("46697a7a"), StandardCharsets.UTF_8);
-    }
 }
