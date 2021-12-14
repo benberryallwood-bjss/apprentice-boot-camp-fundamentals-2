5bfd0406df91e48f1490a1f134e360a582f5516a
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-12-14 15:16:55 +0000

    Change offer on A to 5 for 220

--- exercises/java/src/test/java/checkout/CheckoutTests.java
+++ exercises/java/src/test/java/checkout/CheckoutTests.java
@@ -30,7 +30,7 @@ public class CheckoutTests {
         checkout.scan("A");
         checkout.scan("A");
         checkout.scan("A");
-        assertThat(checkout.total()).isEqualTo(130);
+        assertThat(checkout.total()).isEqualTo(150);
     }
 
     @Test
@@ -43,7 +43,7 @@ public class CheckoutTests {
         checkout.scan("A");
         checkout.scan("A");
         checkout.scan("A");
-        assertThat(checkout.total()).isEqualTo(260);
+        assertThat(checkout.total()).isEqualTo(270);
     }
 
     @Test
@@ -105,30 +105,30 @@ public class CheckoutTests {
         assertThat(checkout.total()).isEqualTo(130);
 
         checkout.scan("A");
-        assertThat(checkout.total()).isEqualTo(160);
+        assertThat(checkout.total()).isEqualTo(180);
 
         checkout.scan("B");
-        assertThat(checkout.total()).isEqualTo(175);
-
-        checkout.scan("C");
         assertThat(checkout.total()).isEqualTo(195);
 
-        checkout.scan("B");
-        assertThat(checkout.total()).isEqualTo(225);
-
         checkout.scan("C");
+        assertThat(checkout.total()).isEqualTo(215);
+
+        checkout.scan("B");
         assertThat(checkout.total()).isEqualTo(245);
 
-        checkout.scan("D");
-        assertThat(checkout.total()).isEqualTo(260);
+        checkout.scan("C");
+        assertThat(checkout.total()).isEqualTo(265);
 
         checkout.scan("D");
-        assertThat(checkout.total()).isEqualTo(275);
+        assertThat(checkout.total()).isEqualTo(280);
 
         checkout.scan("D");
-        assertThat(checkout.total()).isEqualTo(290);
+        assertThat(checkout.total()).isEqualTo(295);
+
+        checkout.scan("D");
+        assertThat(checkout.total()).isEqualTo(310);
 
         checkout.scan("C");
-        assertThat(checkout.total()).isEqualTo(310);
+        assertThat(checkout.total()).isEqualTo(330);
     }
 }
